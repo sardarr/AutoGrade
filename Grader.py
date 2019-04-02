ee65dc8dd79344bd4a7c6b7d29f1c35e137b7c5d
sardar <sardar@gwu.edu>
2019-04-02 00:12:34 -0400

    Latest version til hw3

--- Grader.py
+++ Grader.py
@@ -2,6 +2,22 @@ import os
 import subprocess
 import shutil
 
+
+def assert_mapper(conf):
+    """
+    :param conf: the configuration
+    :return: Dictionary contains the assert index and the assetion line from the assertion file
+    """
+    ind=1
+    asser_table={}
+    asserts=open('Asserts/'+conf['hwID']+'.txt').readlines()
+    for lines in asserts:
+        if 'assert' in lines:
+            asser_table[str(ind)]=lines
+            ind+=1
+    return asser_table
+
+
 def Clean(student,arg):
     """
     :param student: Get the student homework path
@@ -33,10 +49,10 @@ def Compiler(student):
     test = os.listdir(student)
     cmd = 'javac '+student+"/*.java"
     try:
-        os.system(cmd)
-        return True
+        res=subprocess.call(cmd, shell=True)
+        return res
     except:
-        return False
+        return res
 
 
 def Runner(stundet):
@@ -46,17 +62,18 @@ def Runner(stundet):
     except:
         return 'error'
 
-def resParser(result):
-    splited_res=result.split('\n')
+def resParser(result,assert_tbl):
+    splited_res=[msg for msg in result.split('\n') if len(msg)>4]
     passed=0
     failed=0
-    log={}
+    fail_log=""
     for items in splited_res:
         if 'Passed' in items:
             passed+=1
         else:
             failed+=1
-    log={'total':len(splited_res),'passed':passed,'failed':failed,'Grade':passed/len(splited_res)}
+            fail_log+=assert_tbl[items.split(" ")[1]][:-1]+"\t"
+    log={'total':len(splited_res),'passed':passed,'failed':failed,'Grade':passed/len(splited_res),'failed_log':fail_log}
     return log
 
 
@@ -67,20 +84,27 @@ def main():
 
 if __name__ == '__main__':
     grades={}
-    conf={'hwID':'hw3_debug','req':{'OneDimensional2048.java':'OneDimensional.java',}}
+    conf={'hwID':'hw3','req':{'OneDimensional2048.java':'OneDimensional.java',}}
     homework_root='Homework/' + conf['hwID']
     list_of_students=os.listdir(homework_root)
-    for student_dir in list_of_students:
-        student_dir_full_path=os.path.join(homework_root,student_dir)
-        Clean(student_dir_full_path,conf)
-        CopyRequirments(student_dir_full_path,conf)
-        if(Compiler(student_dir_full_path)):
-            res=Runner(student_dir_full_path)
-            if res!='error':
-                grades[student_dir]=resParser(res)
+    results=open(os.path.join('Results',conf['hwID']),'w')
+    assert_tbl=assert_mapper(conf)
+    try:
+        for student_dir in list_of_students:
+            student_dir_full_path=os.path.join(homework_root,student_dir)
+            Clean(student_dir_full_path,conf)
+            CopyRequirments(student_dir_full_path,conf)
+            if(Compiler(student_dir_full_path)==0):
+                res=Runner(student_dir_full_path)
+                if res!='error':
+                    grades[student_dir]=resParser(res,assert_tbl)
+                else:
+                    grades[student_dir] ='Running time error'
             else:
-                grades[student_dir] ='Running time error'
-        else:
-            grades[student_dir]='compile Error'
+                grades[student_dir]='compile Error'
 
-    print(grades)
+            results.write(student_dir+' results '+str(grades[student_dir])+'\n')
+    except:
+        print(student_dir)
+
+results.close()
